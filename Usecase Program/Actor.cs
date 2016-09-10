5b340c261c19b35b4fbbecca3b69caa2c1d12de1
i348098 <joe.vankollenburg@student.fontys.nl>
2016-09-10 14:31:25 +0200

    drawstring toegevoegd, tekent nu de naam van actor

--- Usecase Program/Actor.cs
+++ Usecase Program/Actor.cs
@@ -20,17 +20,23 @@ namespace Usecase_Program
         // Private variables
         private Rectangle _head;
         private Pen _pen;
+        private Brush _brush;
+        private Font drawFont;
         private Point _bodyPoint1;
         private Point _bodyPoint2;
         private Point _armsPoint1;
         private Point _armsPoint2;
         private Point _legPoint1;
-        private Point _legPoint2;
-        private Point _labelPosition;
+        private Point _legPoint2;       
 
         // Static variables
         private static Color _penColor = Color.Black;
         private static int _penWidth = 1;
+
+        private static Color _brushColor = Color.Black;
+
+        private static int _fontsize = 16;
+        private static string _fonttype = "Arial";
 
         private static int _headWidth = 26;
         private static int _headHeight = _headWidth;
@@ -49,6 +55,8 @@ namespace Usecase_Program
             Name = name;
 
             this._pen = new Pen(_penColor, _penWidth);
+            this._brush = new SolidBrush(_brushColor);
+            this.drawFont = new Font(_fonttype, _fontsize);
 
             this._head = new Rectangle(x, y, _headWidth, _headHeight);
             this._bodyPoint1 = new Point(x + (_headWidth / 2), y + _headHeight);
@@ -62,12 +70,13 @@ namespace Usecase_Program
 
         public void Paint(object sender, PaintEventArgs e)
         {
+            
             e.Graphics.DrawEllipse(this._pen, this._head);
             e.Graphics.DrawLine(this._pen, this._bodyPoint1, this._bodyPoint2);
             e.Graphics.DrawLine(this._pen, this._armsPoint1, this._armsPoint2);
             e.Graphics.DrawLine(this._pen, this._bodyPoint2, this._legPoint1);
             e.Graphics.DrawLine(this._pen, this._bodyPoint2, this._legPoint2);
-            e.Graphics.DrawString(Name, _pen, _labelPosition);
+            e.Graphics.DrawString(Name,drawFont,this._brush,x,y +105);
 
 
         }
