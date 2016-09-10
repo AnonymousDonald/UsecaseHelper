2400ae95115fed2d5f51242eaf7815c8940e35e1
Don Mulders <don_mulders@hotmail.com>
2016-09-10 20:07:14 +0200

    Revert "I fix all de thing"
    This reverts commit eadc4a39dbbe17c4d893586dc69b2f983e66eacd.

--- Usecase Program/Main.cs
+++ Usecase Program/Main.cs
@@ -13,13 +13,11 @@ namespace Usecase_Program
     public partial class Main : Form
     {
         private List<Actor> _actorList;
-        private List<Usecase> _usecaseList;
 
         public Main()
         {
             InitializeComponent();
             _actorList = new List<Actor>();
-            _usecaseList = new List<Usecase>();
         }
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
@@ -27,10 +25,6 @@ namespace Usecase_Program
             foreach(Actor actor in _actorList)
             {
                 actor.Paint(sender, e);
-            }
-            foreach(Usecase usecase in _usecaseList)
-            {
-                usecase.PaintElipse(sender, e);
             }
         }
 
@@ -40,34 +34,11 @@ namespace Usecase_Program
             
             if (me != null)
             {
-                if (rbActoren.Checked && rbCreate.Checked) // create actor
+                if (rbActoren.Checked && rbCreate.Checked)
                 {
                     string name = tbName.Text;
                     Actor n = new Actor(me.X, me.Y, name);
                     _actorList.Add(n);
-                    Canvas.Refresh();
-                }
-                if (rbCreate.Checked && rbUsecase.Checked) // create usecase
-                {
-                    string name = tbName.Text;
-                    Usecase u = new Usecase(me.X, me.Y, name);
-                    _usecaseList.Add(u);
-                    Canvas.Refresh();
-                }
-                if (rbSelect.Checked) // selected
-                {
-                    foreach(Actor n in _actorList)
-                    {
-                        n.ActorSelected(me);
-                        if (n.selected)
-                        {
-                            n.GiveThisSomeCoolAsNiggaCollaMang(Color.Red);
-                        }
-                        else
-                        {
-                            n.GiveThisSomeCoolAsNiggaCollaMang(Color.Black);
-                        }
-                    }
                     Canvas.Refresh();
                 }
             }
